002fac334d457d59c5434a89a1a3eda97bfee942
Veronica De Felice <verodefelic@gmail.com>
2023-04-27 17:19:53 -0400

    Updated CSS

---
+++ src/scenes/AddShow.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function AddShow ({setShows}) {
+    const [title, setTitle] = useState('')
+    const [poster, setPoster] = useState('')
+    const [season, setSeason] = useState ('')
+
+    const navigate = useNavigate()
+
+    console.log({title})
+
+    const handleAddShow = (e) => {
+        e.preventDefault()
+
+        fetch("https://tv-shows-api-vdf.web.app/shows", {
+            method: "POST",
+            headers:{ "Content-Type":"application/json"},
+            body: JSON.stringify({title,poster,season})
+        })
+        .then(resp => resp.json())
+        .then(data => {
+             if (data.message){
+                alert(data.message)
+            return
+        }
+
+        setShows(data)
+        navigate("/")
+        })
+
+        .catch(alert)
+        
+    }
+
+    return(
+        <>
+        <h2>AddShow</h2>
+        <form onSubmit ={handleAddShow}>
+            <label htmlFor= "title">Title
+            <input 
+            type="title"
+            value={title}
+            onChange={(e) => {setTitle(e.target.value)}}/>
+                
+        </label>
+
+        <br />
+
+        <label htmlFor="poster">Poster
+            <input
+            type= "text"
+            value= {poster}
+            onChange= {(e) => {setPoster(e.target.value)}}/>
+        </label>
+
+        <br />
+
+        <label htmlFor="season">Season
+            <input
+            type= "text"
+            value= {season}
+            onChange= {(e) => {setSeason(e.target.value)}}/>
+        </label>
+
+        <br />
+
+        <input type="submit" value="Add Show"/>
+    </form>
+  </>
+ )
+}
+
+
+
+
+            
